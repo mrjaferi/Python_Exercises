a543b9dfa250bb9f54d0fee37c41b137cad479c7
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-10 21:24:25 +0330

    Exercise files "Boolean.py" and "IfElse.py" were added.

---
+++ Boolean.py
@@ -0,0 +1,16 @@
+print(2 < 3)
+print(2 > 3)
+print(2 == 3)
+print(2 == 2)
+print(2 != 3)
+print(not 2 == 3)
+print(2 == 3 and 2 == 2)
+print(2 == 3 or 2 == 2)
+
+print('---------------')
+
+t = True
+f = False
+
+print(t and f)
+print(t or f)
---
+++ IfElse.py
@@ -0,0 +1,41 @@
+woroodi=input('Marhale 1 yaa 2 ?')
+
+if woroodi == '1':
+    x = input('Please enter the amount :')
+    x=int(x)
+
+    if x > 10:
+        print('The entered number is greater than 10 .')
+        print('----------------------------------------')
+    elif x == 10:
+        print('The entered number is equal to 10 .')
+        print('----------------------------------------')
+    elif x<10:
+        print('The entered number is less than 10 .')
+        print('----------------------------------------')
+    else:
+        print('The entered phrase is incorrect.')
+        print('----------------------------------------')
+
+
+    print('----------------------------------------')
+elif woroodi == '2':
+    z=input('Please enter the amount :')
+    z=int(z)
+
+    if z>0:
+        print('The entered number is greater than 0 .')
+        print('----------------------------------------')
+    elif z==0:
+        print('The entered number is equal to 0 .')
+        print('----------------------------------------')
+    elif z<0:
+        print('The entered number is less than 0 .')
+        print('----------------------------------------')
+    else:
+        print('The entered phrase is incorrect.')
+        print('----------------------------------------')
+
+    print('----------------------------------------')
+else:
+    print('The entered phrase is incorrect.')
