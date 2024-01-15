50dd0d7d4bd190146c2ad0caee7c02213bed0ecd
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-15 19:38:32 +0330

    "ForIteration.py" file was added

---
+++ ForIteration.py
@@ -0,0 +1,22 @@
+
+
+friendsName = ['yasin', 'ali', 'hasan']
+count = 0
+
+for i in friendsName:
+    print('salam', i)
+    count += 1
+
+
+print('----------------\nI said', count, 'hellos')
+
+number = input('Enter number: ')
+number = int(number)
+
+for i in range(number, 10):
+    print(i)
+    number += 1
+
+
+for i in range(1, 10+1):
+    print(i)
