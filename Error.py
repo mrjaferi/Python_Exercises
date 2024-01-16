7806d76e65f0612148950e367ea9d4a0ecba3cf4
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-16 21:09:42 +0330

    "Error.py" file was added

---
+++ Error.py
@@ -0,0 +1,10 @@
+# Types of errors:
+# 1. Syntax errors خطای دستوری قبل از اجرا
+# 2. Runtime errors خطای منطقی در زمان اجرا
+
+# Syntax: خطای دستوری در تکه کد زیر
+# print 123
+
+# Runtime: تقسیم بر صفر تعریف نشده
+# 11/0
+
