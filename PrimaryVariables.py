6442c0b7ddc4cb6764855024488301ba85987661
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-17 18:19:55 +0330

    "PrimaryVariables.py" file was added

---
+++ PrimaryVariables.py
@@ -0,0 +1,10 @@
+# Dependencies of variables:
+# 1. CPU: مرکز پردازش برنامه
+# 2. RAM: حافظه با دسترسی تصادفی
+# 3. I/O: ورودی ها و خروجی ها مانند ماوس و کیبورد و ...
+
+# The main characteristics of the variables:
+# 1. type
+# 2. size
+# 3. name
+# 4. address فقط برای یادگیری
