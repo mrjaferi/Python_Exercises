1f3a18cdcf4bd6aba6f539910bbd55301964cf44
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-17 19:43:43 +0330

    "PrintIntegers.py" file was added

---
+++ PrintIntegers.py
@@ -0,0 +1,11 @@
+# GOD
+
+primeNumber = 0
+primeNumber = int(primeNumber)
+
+inputNumber = input('Enter your number: ')
+inputNumber = int(inputNumber)
+
+while primeNumber < inputNumber:
+    print(primeNumber)
+    primeNumber += 1
