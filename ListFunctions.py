bec7ce9e4e76662ef3237ed217e0cf10ae7fc162
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-20 22:12:33 +0330

    "ListFunctions.py" file was added

---
+++ ListFunctions.py
@@ -0,0 +1,16 @@
+myCourses=['python','csharp','asp.net']
+print(myCourses)
+
+myCourses.append('django')
+print(myCourses)
+
+myCourses.extend(['java','jquery'])
+print(myCourses)
+
+# گسترش - آخر لیست
+myCourses.extend(['java','jquery'])
+print(myCourses)
+
+myCourses.insert(0,'php')
+print(myCourses)
+
