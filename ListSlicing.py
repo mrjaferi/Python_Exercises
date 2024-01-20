ba1dc49c29c775e7ba1a213a76c5dbbca5f7b023
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-20 22:12:14 +0330

    "ListSlicing.py" file was added

---
+++ ListSlicing.py
@@ -0,0 +1,16 @@
+myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+selectedNumber = myNumbers[1]
+
+selectedList = myNumbers[1:8:2]
+
+selectedList = myNumbers[2:]
+
+selectedList = myNumbers[-5:]
+
+selectedList = myNumbers[:9]
+print(selectedList)
+
+copyOfNumbers = myNumbers[0:]
+copyOfNumbers = myNumbers[:]
+print(copyOfNumbers)
