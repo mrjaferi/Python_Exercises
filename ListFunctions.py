197ca7853d60e9158e0fe8cbe36e19c72bc6fcfe
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-01-28 22:03:21 +0330

    I added more list functions.

--- ListFunctions.py
+++ ListFunctions.py
@@ -1,16 +1,42 @@
-myCourses=['python','csharp','asp.net']
+from cgi import print_arguments
+
+
+myCourses = ['python', 'csharp', 'asp.net']
 print(myCourses)
 
 myCourses.append('django')
 print(myCourses)
 
-myCourses.extend(['java','jquery'])
+myCourses.extend(['java', 'jquery'])
 print(myCourses)
 
 # گسترش - آخر لیست
-myCourses.extend(['java','jquery'])
+myCourses.extend(['java', 'jquery'])
 print(myCourses)
 
-myCourses.insert(0,'php')
+myCourses.insert(0, 'php')
 print(myCourses)
 
+print('-----------------------------------')
+
+myFriends = ['yasin', 'hasti', 'abbas', 'yasin', 'sobhan']
+
+# myFriends.clear()
+
+# myFriends.pop(1)
+
+# myFriends.remove('hasti')
+
+# print(myFriends.index('yasin'))
+
+# print(myFriends.index('yasin', 1, 4))
+
+# print(myFriends.count('yasin'))
+
+# print(myFriends.reverse())
+
+myFriends.sort()
+print(myFriends)
+
+friend = " - ".join(myFriends)
+print(friend)
