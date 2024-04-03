89ac86bc7cd729bf6bff7424f4fab52106083ef9
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-04-03 21:00:06 +0330

    "ProfessionalChess.py" file was added

---
+++ ProfessionalChess.py
@@ -0,0 +1,10 @@
+shah, vazir, rokh, fil, asb, sarbaaz = input().split(" ")
+
+shah = 1 - int(shah)
+vazir = 1 - int(vazir)
+rokh = 2 - int(rokh)
+fil = 2 - int(fil)
+asb = 2 - int(asb)
+sarbaaz = 8 - int(sarbaaz)
+
+print(shah, vazir, rokh, fil, asb, sarbaaz)
