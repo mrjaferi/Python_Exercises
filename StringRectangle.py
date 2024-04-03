ae2e79a7c22dbb3e07f14533e7120d5a22e69908
Yasin Jaferi <mrjaferi.dev@gmail.com>
2024-04-03 21:03:20 +0330

    "stringRectangle.py" file was added

---
+++ StringRectangle.py
@@ -0,0 +1,10 @@
+char = input()
+n = int(input())
+
+space = " "
+x = n - 2
+print(n * char)
+print(char, space * x, char)
+print(char, space * x, char)
+print(char, space * x, char)
+print(n * char)
